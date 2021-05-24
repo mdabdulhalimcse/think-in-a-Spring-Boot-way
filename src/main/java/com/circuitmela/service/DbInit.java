e34292f80ad93a04dc6d891f4cb5880288634de3
mdabdulhalimcse <mdabdulhalimcse@users.noreply.github.com>
2021-05-24 09:16:52 +0600

    runtime table created

---
+++ src/main/java/com/circuitmela/service/DbInit.java
@@ -0,0 +1,25 @@
+package com.circuitmela.service;
+
+
+import com.circuitmela.model.Employee;
+import com.circuitmela.repository.EmployeeRepository;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class DbInit {
+    private  final EmployeeRepository employeeRepository;
+    public DbInit(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+    }
+
+//    @PostConstruct
+    public void init(){
+        Employee employee = new Employee();
+        employee.setName("Abdul Halim");
+        employee.setAddress("Dhaka");
+        employee = employeeRepository.save(employee);
+        System.out.println("E ID = "+employee.getId());
+    }
+}
